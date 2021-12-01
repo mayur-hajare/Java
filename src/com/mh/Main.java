caf8b9ffa1a517b8455f815f5359ec8f27fb4325
MH <myur.hajare@gmail.com>
2021-12-01 08:42:00 +0530

    3 no add

---
+++ src/com/mh/Main.java
@@ -0,0 +1,15 @@
+package com.mh;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Sum of Three Numbers.");
+
+        int a=10,b=30,c=50;
+        System.out.println(a+b+c);
+
+    }
+}
