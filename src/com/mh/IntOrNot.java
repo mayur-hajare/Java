b0e6e3e9000ed3bd399a02423af3f8cbd22dbdf1
MH <myur.hajare@gmail.com>
2021-12-03 14:42:53 +0530

    check Int Or Not

--- src/com/mh/IntOrNot.java
+++ src/com/mh/IntOrNot.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class IntOrNot {
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Any Number : ");
-        boolean num=scanner.hasNextInt();
-        if (num){
+        boolean num = scanner.hasNextInt();
+        if (num) {
             System.out.println("Enter Number is Integer.");
-        }else{
+        } else {
             System.out.println("Enter Number is Not Integer.");
         }
     }
