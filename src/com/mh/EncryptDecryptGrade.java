9b1e09f15f2deb39bb7c17a31b825e19dbfb3263
MH <myur.hajare@gmail.com>
2021-12-03 16:46:07 +0530

    EncryptDecryptGrade

---
+++ src/com/mh/EncryptDecryptGrade.java
@@ -0,0 +1,20 @@
+package com.mh;
+
+import java.util.Scanner;
+
+public class EncryptDecryptGrade {
+    public static void main(String[] args) {
+
+        //W.A.P to encrypt a grade by adding 8 to it. Decrypt it to show the correct Grade.
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter Your Grade : ");
+        float grade=scanner.nextFloat();
+        grade=grade+8;
+        System.out.println("Encrypted Grade is : "+grade);
+        grade=grade-8;
+        System.out.println("Decrypted Grade is : "+grade);
+
+
+    }
+}
