91d17e0470a581f58a2b504a2d20950f5b2da1dd
MH <myur.hajare@gmail.com>
2021-12-04 09:58:56 +0530

    String Practice set

---
+++ src/com/mh/StringPrSet.java
@@ -0,0 +1,35 @@
+package com.mh;
+
+import java.util.Scanner;
+
+public class StringPrSet {
+    public static void main(String[] args) {
+
+        // string to lower case
+        String str = "MAYUR";
+        str = str.toLowerCase();
+        System.out.println(str);
+
+        // replace whitespaces to underscore
+
+        String str1 = "Hello Good Morning..!";
+        str1 = str1.replace(' ', '_');
+        System.out.println(str1);
+
+        // Fill in a letter template
+        // Dear, <Name> Thanks A lot...!
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Your Name : ");
+        String str2 = scanner.next();
+        System.out.print("Dear, " + str2 + " Thanks A lot..!");
+
+        // Find out the double and triple space from string
+
+        String str3 = "Find out the double and triple  space from   string";
+        System.out.println(str3.indexOf("  "));
+        System.out.println(str3.indexOf("   "));
+
+
+    }
+}
