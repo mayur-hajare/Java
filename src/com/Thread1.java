5353c35568be13317420a0eab979165bcf501a46
MH <myur.hajare@gmail.com>
2021-12-25 20:30:03 +0530

    Thread Practical

---
+++ src/com/Thread1.java
@@ -0,0 +1,39 @@
+package com;
+
+import java.util.SortedMap;
+
+class Welcome extends Thread {
+
+    public void run() {
+        // while (1 == 1)
+        System.out.println("Welcome");
+    }
+
+}
+
+class GM extends Thread {
+    public void run() {
+        // while (1 == 1)
+        System.out.println("Good Morning...!");
+    }
+}
+
+public class Thread1 {
+    public static void main(String[] args) {
+
+        Welcome welcome = new Welcome();
+        GM gm = new GM();
+        gm.setPriority(Thread.MAX_PRIORITY);
+        welcome.setPriority(Thread.NORM_PRIORITY);
+        welcome.start();
+        System.out.println(welcome.getState());
+        gm.start();
+        try {
+            welcome.wait(300);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
